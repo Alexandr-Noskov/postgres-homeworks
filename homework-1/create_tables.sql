b98409807b99aee72cdc1d398755729150fba9fc
Alexandr Noskov <san9nosko8@gmail.com>
2023-06-27 20:45:21 +0300

    Sql-код для создания таблиц скопировать в файл create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,35 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+# tables 1
+(
+    customer_id int PRIMARY KEY,
+    company_name varchar(100) NOT NULL,
+    contact_name varchar(100) NOT NULL
+);
+
+#INSERT INTO customers VALUES ();
+# добавление данных
+
+#SELECT FROM * customers
+# выборка записей
+
+CREATE TABLE employees
+# tables 2
+(
+    employee_id int PRIMARY KEY,
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL,
+    title varchar(100) NOT NULL,
+    birth_date date,
+    notes text
+)
+
+CREATE TABLE orders
+# tables 3
+(
+    order_id int PRIMARY KEY,
+    customer_id int REFERENCES customers (customer_id) NOT NULL,
+    employee_id int REFERENCES employees (employee_id),
+    order_date varchar(100) NOT NULL,
+    ship_city varchar(100) NOT NULL
+)
